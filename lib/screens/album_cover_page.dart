a9626b1abdd0385cfb62d69a278d9e5bfa1d125c
harlock024 <hadith024@gmail.com>
2024-06-13 04:49:00 -0600

    vote pre

--- lib/screens/album_cover_page.dart
+++ lib/screens/album_cover_page.dart
@@ -1,9 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import '../widgets/album_cover_list.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AlbumCoverPage extends StatelessWidget {
-  const AlbumCoverPage({super.key});
+  const AlbumCoverPage({Key? key}) : super(key: key);
+
+  Future<String?> _getAuthToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('authToken');
+  }
+
+  Future<void> createVote(int albumCoverId) async {
+    final authToken = await _getAuthToken();
+    if (authToken != null) {
+      const  url = 'http://127.0.0.1:8000/graphql/';
+      final headers = <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'JWT $authToken',
+      };
+      final body = jsonEncode({
+        'query': '''
+          mutation CreateVote(\$albumCoverId: Int!) {
+            createVote(albumCoverId: \$albumCoverId) {
+              id
+            }
+          }
+        ''',
+        'variables': {
+          'albumCoverId': albumCoverId,
+        },
+      });
+
+      final response = await http.post(Uri.parse(url), headers: headers, body: body);
+      if (response.statusCode == 200) {
+        final jsonResponse = jsonDecode(response.body);
+        // ignore: avoid_print
+        print('Mutation response: $jsonResponse');
+        // Handle the response as needed
+      } else {
+        // ignore: avoid_print
+        print('Request failed with status: ${response.statusCode}');
+                // ignore: avoid_print
+        print('Error message: ${response.body}');
+        // Handle the error as needed
+      }
+    } else {
+              // ignore: avoid_print
+      print('Error: No authToken available');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,12 +98,19 @@ class AlbumCoverPage extends StatelessWidget {
               final coverUrl = album['coverUrl'];
               final postedBy = album['postedBy']['username'];
               final votes = album['votes'].length;
-              return AlbumCoverList(
-                Artist: artist,
-                Album: albumName,
-                ImageUrl: coverUrl,
-                postedBy: postedBy,
-                votes: votes,
+              final albumCoverId = album['id']; // Obtén el ID del álbum para la votación
+
+              return Card(
+                margin: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Image.network(coverUrl),
+                  title: Text(albumName),
+                  subtitle: Text('By $artist\nPosted by: $postedBy\nVotes: $votes'),
+                  trailing: ElevatedButton(
+                    onPressed: () => createVote(albumCoverId),
+                    child: const Text('Vote'),
+                  ),
+                ),
               );
             },
           );
